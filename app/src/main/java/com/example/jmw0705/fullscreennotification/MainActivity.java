8c27d54c1b5fd8599222e8075a66873cd4527a43
rettwalkerwork <Rett.Walker@daugherty.com>
2016-08-26 09:36:54 -0400

    Created 4 buttons to link to different notifications
    4 buttons created that will link to a different notification design for the POC and set up function to create notification with the same settings just new layouts

--- app/src/main/java/com/example/jmw0705/fullscreennotification/MainActivity.java
+++ app/src/main/java/com/example/jmw0705/fullscreennotification/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jmw0705.fullscreennotification;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -11,6 +12,7 @@ import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RemoteViews;
 
 import com.example.jmw0705.fullscreennotification.helps.CustomIntentService;
@@ -26,15 +28,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        Button not1 = (Button) findViewById(R.id.notification1);
+        Button not2 = (Button) findViewById(R.id.notification2);
+        Button not3 = (Button) findViewById(R.id.notification3);
+        Button not4 = (Button) findViewById(R.id.notification4);
+
+        not1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent("com.example.jmw0705.fullscreennotification.USER_ACTION");
-                sendBroadcast(i);
+                sendNotification(R.layout.full_screen_notificiation);
             }
         });
+
     }
+    public void sendNotification(int layoutToUse){
+        RemoteViews remoteView = new RemoteViews("com.example.jmw0705.fullscreennotification",layoutToUse);
+        Intent newIntent = new Intent(getBaseContext(),AlertMessage.class);
+        PendingIntent pendingIntent = PendingIntent.getService(getBaseContext(),0,newIntent,0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext());
+        builder.setSmallIcon(R.drawable.icon)
+                .setOngoing(true)
+                .setCategory(Notification.CATEGORY_ALARM)
+                .setFullScreenIntent(pendingIntent,true)
+                .setContent(remoteView)
+                .setContentIntent(pendingIntent)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setVibrate(new long[] {500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500})
+                .setCustomHeadsUpContentView(remoteView);
 
 
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        int temp = 001;
+        notificationManager.notify(temp,builder.build());
+
+    }
 }
