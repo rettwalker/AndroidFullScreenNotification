7a5d15fdfbc09aa5182afb67bb79a7e58d907b7d
rettwalkerwork <Rett.Walker@daugherty.com>
2016-08-29 15:23:47 -0400

    Changes to Layout and working towards full screen

--- app/src/main/java/com/example/jmw0705/fullscreennotification/AlertMessage.java
+++ app/src/main/java/com/example/jmw0705/fullscreennotification/AlertMessage.java
@@ -9,7 +9,9 @@ import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
+
+import android.view.View;
+
 
 
 public class AlertMessage extends AppCompatActivity {
@@ -21,6 +23,43 @@ public class AlertMessage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemUI();
+        prepareActivityForAlert();
+
+        if(audioManager.getMode()!=AudioManager.MODE_IN_CALL){
+            //mMediaPlayer.start();
+        }
+        vibrator.vibrate(999999999);
+
+
+        setContentView(R.layout.activity_alert_message);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUI();
+    }
+
+    // This snippet hides the system bars.
+    private void hideSystemUI() {
+        View mDecorView = getWindow().getDecorView();
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+
+
+
+    public void prepareActivityForAlert(){
         mMediaPlayer = new MediaPlayer();
         Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
         mMediaPlayer = MediaPlayer.create(this,soundUri);
@@ -31,13 +70,7 @@ public class AlertMessage extends AppCompatActivity {
         Log.d("Volume", currentVolume+"");
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),AudioManager.AUDIOFOCUS_GAIN);
 
-        if(audioManager.getMode()!=AudioManager.MODE_IN_CALL){
-            //mMediaPlayer.start();
-        }
-        vibrator.vibrate(999999999);
 
-
-        setContentView(R.layout.activity_alert_message);
     }
 
     @Override
